65955448598e75860a807efac07f75167cd21782
Sukit Bunsiri <100616257+Neolization@users.noreply.github.com>
2022-07-31 20:51:11 +0700

    Add project section

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,15 +1,114 @@
 import React from "react";
+import styled from "styled-components";
+import { Github } from "@icons-pack/react-simple-icons";
+import { VscGithub } from "react-icons/vsc";
+import { ExternalLinkIcon } from "@heroicons/react/solid";
+import { HiOutlineExternalLink } from "react-icons/hi";
+
+import "../../global.sass";
 
 export function ProjectCard(props) {
+  const TechList = styled.ul`
+    display: flex;
+    list-style: none;
+    padding: 0px;
+    & li {
+      
+      padding: 0px 10px;
+    }
+  `;
+
+  const ProjectCard = styled.div`
+    display: grid;
+    grid-template-columns: 25% 25% 25% 25%;
+    margin-bottom: 100px;
+    
+  `;
+
+  const ProjectDetail = styled.div`
+    background-color: #1B2027;
+    display: flex;
+    flex-direction: column;
+    grid-column: 3 / 5;
+    grid-row: 2 / 3;
+    z-index: 1;
+    padding: 30px;
+    box-shadow: 0 19px 38px black;
+
+    @media only screen and (max-width: 768px) {
+      grid-column: 1/5;
+      grid-row: 4;
+    }
+  `;
+
+  const Title = styled.h3`
+    font-weight: bold;
+  `;
+
+  const Content = styled.p`
+    line-height: 1.5;
+  `;
+
+  const Menu = styled.div``;
+
+  const Img = styled.div`
+    grid-column: 1 / 4;
+    grid-row: 1 / 4;
+
+    @media only screen and (max-width: 768px) {
+      grid-column: 1/5;
+      grid-row: 1/3;
+    }
+
+    & img {
+      max-width: 100%;
+    }
+  `;
+
+  const StyledGithub = styled(VscGithub)`
+    width: 30px;
+    height: 30px;
+    fill: #b9bbb3;
+
+    &:hover {
+      fill: #ff7b72;
+    }
+
+    padding: 0px 5px;
+  `;
+  const StyledExternalLinkIcon = styled(HiOutlineExternalLink)`
+    width: 30px;
+    height: 30px;
+    color: #b9bbb3;
+    padding: 0px 5px;
+
+    &:hover {
+      color: #ff7b72;
+    }
+  `;
+
   return (
-    <div className="ProjectCard">
-      <div className="project-content">
-        <h3>{props.title}</h3>
-        <p>{props.content}</p>
-      </div>
-      <div className="project-img">
-        <img src={props.img} />
-      </div>
-    </div>
+    <ProjectCard>
+      <ProjectDetail>
+        <Title>{props.title}</Title>
+        <Content>{props.content}</Content>
+        <TechList>
+          {props.techlist.map(({ tech, href }) => (
+            <a target="_blank" className="underline-hover-effect" href={href}><li>{tech}</li></a>
+          ))}
+        </TechList>
+        <Menu>
+          <a href={props.github} target="_blank">
+            <StyledGithub />
+          </a>
+          <a href={props.demo} target="_blank">
+            <StyledExternalLinkIcon />
+          </a>
+        </Menu>
+      </ProjectDetail>
+      <Img>
+        <img src={props.img}></img>
+      </Img>
+    </ProjectCard>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,26 +1,85 @@
 import React from "react";
 import { ProjectCard } from "../ProjectCard/ProjectCard";
-import Jammming from '../../assets/ProjectSS/Jammming/jammmingSS.png'
+import Jammming from "../../assets/ProjectSS/Jammming/jammming-sukitb.surge 1 (1).png";
+import Colmar from "../../assets/ProjectSS/Colmar-academy/desktop.png"
+import { Anchor } from "../Anchor/Anchor";
 
 export function Projects() {
+  [];
 
-
-    const projectsData = [
+  const projectsData = [
+    {
+      img: Jammming,
+      title: `Jammming`,
+      content: (
+        <>
+          A web application, built with React.js, that allows users to search
+          the Spotify library, create a custom playlist, and then save the
+          playlist to their Spotify accounts.
+        </>
+      ),
+      techlist: [
         {
-         img: Jammming,
-         title: `Jammming`,
-         content: `Test Test`,
+          tech: `React`,
+          href: `https://reactjs.org/`,
         },
-    ]
+        {
+          tech: `Spotify API`,
+          href: `https://developer.spotify.com/documentation/web-api/`,
+        },
+        {
+          tech: `Surge`,
+          href: `https://surge.sh/`,
+        },
+      ],
+      github: `https://github.com/sukitb/jammming`,
+      demo: `https://jammming-sukitb.surge.sh/`,
+    },
+    {
+      img: Colmar,
+      title: `COLMAR ACADEMY`,
+      content: (
+        <>
+          Responsive landing page for school. Using HTML, CSS, Bootstrap and
+          GitHub pages.
+        </>
+      ),
+      techlist: [
+        {
+          tech: `HTML`,
+          href: `https://developer.mozilla.org/en-US/docs/Web/HTML`,
+        },
+        {
+          tech: `CSS`,
+          href: `https://developer.mozilla.org/en-US/docs/Web/CSS`,
+        },
+        {
+          tech: `Bootstrap`,
+          href: `https://getbootstrap.com/`,
+        },
+        {
+            tech: `GitHub Pages`,
+            href: `https://docs.github.com/en/pages`,
+          },
+      ],
+      github: `https://github.com/sukitb/colmar-academy`,
+      demo: `https://sukitb.github.io/colmar-academy/`,
+    },
+  ];
 
-    return (
-        <div className="Projects">
-            {projectsData.map(({ img, title, content }) => (
-                <ProjectCard
-                img={img}
-                title={title}
-                content={content} />
-            ))}
-        </div>
-    )
+  return (
+    <div className="Projects">
+        <h2 className="highlight">PROJECTS</h2>
+      {projectsData.map(({ img, title, content, techlist, github, demo }) => (
+        <ProjectCard
+          img={img}
+          title={title}
+          content={content}
+          techlist={techlist}
+          github={github}
+          demo={demo}
+        />
+      ))}
+    </div>
+  );
 }
