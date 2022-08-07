9f0980ec4846077365952c30c78dd7a1b5563858
Sukit Bunsiri <100616257+Neolization@users.noreply.github.com>
2022-08-07 20:18:17 +0700

    hotfix - remove packages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: '8000'
-  }
-})
+
